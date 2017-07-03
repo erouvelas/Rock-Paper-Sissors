3b74e912c79d037a368e5b4999f9386d3fdacef1
erouvelas <erouvelas@users.noreply.github.com>
2017-07-03 15:58:16 -0400

    Add files via upload

--- RockPaperSissors.py
+++ RockPaperSissors.py
@@ -1,10 +1,31 @@
-print "Rock-Paper-Sissors Machine Says..."
-
 import random
-x = random.randint(0,2)
+while True==True:
 
-a=["Rock","Paper","Sissors"]
-choice=a[x]
-
-
-print choice
+    options = ["Rock","Paper","Scissors"]
+    computer = random.choice(options)
+    human = raw_input("Pick either Rock, Paper or Scissors: ")
+    human = human.lower()
+    if human == 'rock' or 'paper' or 'scissors':
+        print "The computer chose %s while you chose %s " % (computer,human)
+    if human == 'rock':
+        if computer == 'Rock':
+            print 'Tie Game'
+            
+        elif computer == 'Paper':
+            print 'Computer Wins'
+        else:
+            print 'You Win'
+    if human == 'paper':
+        if computer == 'Rock':
+            print 'You Win'
+        elif computer == 'Paper':
+            print 'Tie Game'
+        else:
+            print 'Computer Wins'
+    if human == 'scissors':
+        if computer == 'Rock':
+            print 'Computer Wins'
+        elif computer == "Paper":
+            print "You Win"
+        else: 
+            print "Tie Game"
